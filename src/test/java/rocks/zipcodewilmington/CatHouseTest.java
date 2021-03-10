12fe0a3bf846ccfdd82e00fd04eebe6e314ec2cd
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-09 22:00:37 -0500

    as far as I know, all tests pass

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,12 +1,94 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.AnimalWarehouse;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
+    @Test
+    public void addCatTest() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Cat ranCat = new Cat(newName, givenBirthdate, givenId);
+
+        //When
+        CatHouse.add(ranCat);
+
+        //Then
+        Assert.assertEquals((long)CatHouse.getNumberOfCats(), 1l);
+    }
     // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void testRemove() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Cat ranCat = new Cat(newName, givenBirthdate, givenId);
+
+        //When
+        CatHouse.add(ranCat);
+        CatHouse.remove(12);
+
+        //Then
+        Assert.assertEquals((long)CatHouse.getNumberOfCats(), 0);
+    }
     // TODO - Create tests for `void remove(Cat cat)`
+    @Test
+    public void testRemoveByCat() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Cat ranCat = new Cat(newName, givenBirthdate, givenId);
+
+        //When
+        CatHouse.add(ranCat);
+        CatHouse.remove(ranCat);
+
+        //Then
+        Assert.assertEquals((long)CatHouse.getNumberOfCats(), 0);
+    }
     // TODO - Create tests for `Cat getCatById(Integer id)`
+    @Test
+    public void testGetById() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+
+        //When
+        Cat ranCat = new Cat(newName, givenBirthdate, givenId);
+        CatHouse.add(ranCat);
+
+        //Then
+        Assert.assertEquals(ranCat, CatHouse.getCatById(12));
+    }
     // TODO - Create tests for `Integer getNumberOfCats()`
+    @Test
+    public void numOfCats() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+
+        //When
+        Cat ranCat = new Cat(newName, givenBirthdate, givenId);
+        CatHouse.add(ranCat);
+
+        //Then
+        Assert.assertEquals(1l, (long)CatHouse.getNumberOfCats());
+    }
 }
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,8 +1,11 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 
 import java.util.Date;
@@ -12,10 +15,82 @@ import java.util.Date;
  */
 public class DogHouseTest {
     // TODO - Create tests for `void add(Dog dog)`
+    @Test
+    public void addDogTest() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Dog ranDog = new Dog(newName, givenBirthdate, givenId);
+
+        //When
+        DogHouse.add(ranDog);
+
+        //Then
+        Assert.assertEquals(1, (long)DogHouse.getNumberOfDogs());
+    }
     // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeDogIdTest() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Dog ranDog = new Dog(newName, givenBirthdate, givenId);
+
+        //When
+        DogHouse.add(ranDog);
+        DogHouse.remove(12);
+
+        //Then
+        Assert.assertEquals(0, (long)DogHouse.getNumberOfDogs());
+    }
     // TODO - Create tests for `void remove(Dog dog)`
+    @Test
+    public void removeDogTest() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+        Dog ranDog = new Dog(newName, givenBirthdate, givenId);
+
+        //When
+        DogHouse.add(ranDog);
+        DogHouse.remove(ranDog);
+
+        //Then
+        Assert.assertEquals(0, (long)DogHouse.getNumberOfDogs());
+    }
     // TODO - Create tests for `Dog getDogById(Integer id)`
+    @Test
+    public void testGetById() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+
+        //When
+        Dog ranDog = new Dog(newName, givenBirthdate, givenId);
+        DogHouse.add(ranDog);
+
+        //Then
+        Assert.assertEquals(ranDog, DogHouse.getDogById(12));
+    }
     // TODO - Create tests for `Integer getNumberOfDogs()`
+    @Test
+    public void numOfDogs() {
+        //Given
+        String newName = "Leon";
+        Date givenBirthdate = new Date();
+        Integer givenId = 12;
+
+        //When
+        Dog ranDog = new Dog(newName, givenBirthdate, givenId);
+        DogHouse.add(ranDog);
+
+        //Then
+        DogHouse.getNumberOfDogs();
+    }
 
     @Test
     public void testGetNumberOfDogs() {
